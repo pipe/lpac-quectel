2b5d94e813cc8c59952a44e76e2fef391f148a0a
T H Panton <thp@westhawk.co.uk>
2024-06-26 15:49:25 +0200

    Stepped down to raw io to see if that's the problem.

--- driver/apdu/at.c
+++ driver/apdu/at.c
@@ -5,87 +5,155 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdbool.h>
 
 #include <euicc/interface.h>
 #include <euicc/hexutil.h>
 
-static FILE *fuart;
 static int logic_channel = 0;
+static int uart_fd =0;
+static int at_debug =0;
+static char buffer[10240];
+
 
 static int at_expect(char **response, const char *expected)
 {
-    char buffer[1024];
-
+    int bytesAv = 0;
+    char * ip;
+    char *ex;
+    char *eol;
+    int done = false;
+    int ret =0;
+    int red =0;
     if (response)
         *response = NULL;
-
-    while (1)
+    ip = buffer;
+    while (!done)
     {
-        fgets(buffer, sizeof(buffer), fuart);
-        buffer[strcspn(buffer, "\r\n")] = 0;
-        if (getenv("AT_DEBUG"))
-            printf("AT_DEBUG: %s\r\n", buffer);
-        if (strcmp(buffer, "ERROR") == 0)
+        if ( ioctl (uart_fd,FIONREAD,&bytesAv) < 0 )
         {
-            return -1;
+            ret = -1;
+            done = true;
         }
-        else if (strcmp(buffer, "OK") == 0)
+        else if (bytesAv == 0)
         {
-            return 0;
+            usleep(100000);
         }
-        else if (expected && strncmp(buffer, expected, strlen(expected)) == 0)
+        else
         {
-            if (response)
-                *response = strdup(buffer + strlen(expected));
+            if ((sizeof(buffer)-(ip-buffer)) > bytesAv)
+            {
+                red=read(uart_fd,ip,bytesAv);
+                if (red <1)
+                {
+                    ret = -1;
+                    done = true;
+                }
+                else
+                {
+                    if (at_debug == 1)
+                    {
+                        write(1,"<",1);
+                        write(1,ip,red);
+                    }
+                    ip += red;
+                    *(ip+1) = '\0';
+                }
+                if (strstr(buffer, "ERROR") != NULL)
+                {
+                    ret = -1;
+                    done = true;
+                }
+                else if(strstr(buffer, "OK") != NULL)
+                {
+                    ret = 0;
+                    done = true;
+                    if ((expected != NULL)&& ((ex = strstr(buffer, expected)) != NULL))
+                    {
+                        if (response)
+                        {
+                            eol = strstr(ex,"\r\n");
+                            *eol = '\0';
+                            *response = strdup(ex + strlen(expected));
+                            if (at_debug ==1){
+                                write(1,"=",1);
+                                write(1,*response,strlen(*response));
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ret = -1;
+                done = true;
+            }
         }
     }
-    return 0;
+    return ret;
 }
 
+static int writeString(char * st){
+    if (at_debug >0)
+    {
+        write(1,">",1);
+        write(1,st,strlen(st));
+    }
+    write(uart_fd,st,strlen(st));
+}
 static int apdu_interface_connect(struct euicc_ctx *ctx)
 {
     const char *device;
-
+    
     logic_channel = 0;
-
+    
+    if (getenv("AT_DEBUG"))
+        at_debug = 1;
+    
     if (!(device = getenv("AT_DEVICE")))
     {
-        device = "/dev/ttyUSB0";
+        device = "/dev/ttyUSB2";
     }
-
-    fuart = fopen(device, "r+");
-    if (fuart == NULL)
+    uart_fd = open(device, O_RDWR);
+    if (uart_fd < 0)
     {
         fprintf(stderr, "Failed to open device: %s\n", device);
         return -1;
     }
-
-    fprintf(fuart, "AT+CCHO=?\r\n");
+    
+    writeString("AT+CMEE=2\r\n");
+    if (at_expect(NULL, NULL))
+    {
+        fprintf(stderr, "Device refused extended error codes\n");
+        return -1;
+    }
+    writeString("AT+CCHO=?\r\n");
     if (at_expect(NULL, NULL))
     {
         fprintf(stderr, "Device missing AT+CCHO support\n");
         return -1;
     }
-    fprintf(fuart, "AT+CCHC=?\r\n");
+    writeString("AT+CCHC=?\r\n");
     if (at_expect(NULL, NULL))
     {
         fprintf(stderr, "Device missing AT+CCHC support\n");
         return -1;
     }
-    fprintf(fuart, "AT+CGLA=?\r\n");
+    writeString("AT+CGLA=?\r\n");
     if (at_expect(NULL, NULL))
     {
         fprintf(stderr, "Device missing AT+CGLA support\n");
         return -1;
     }
-
+    
     return 0;
 }
 
 static void apdu_interface_disconnect(struct euicc_ctx *ctx)
 {
-    fclose(fuart);
-    fuart = NULL;
+    close(uart_fd);
     logic_channel = 0;
 }
 
@@ -95,21 +163,33 @@ static int apdu_interface_transmit(struct euicc_ctx *ctx, uint8_t **rx, uint32_t
     int ret;
     char *response = NULL;
     char *hexstr = NULL;
-
+    int mxal =0;
+    char *op;
+    char *wout;
+    char *wp;
+    
+    
     *rx = NULL;
     *rx_len = 0;
-
+    
     if (!logic_channel)
     {
         return -1;
     }
-
-    fprintf(fuart, "AT+CGLA=%d,%u,\"", logic_channel, tx_len * 2);
+    
+    mxal = 15+(2*tx_len)+2+10;
+    wout = calloc((mxal+1)/2,2);
+    wp = wout;
+    
+    wp += snprintf(wp, mxal,"AT+CGLA=%d,%u,\"", logic_channel, tx_len * 2);
     for (uint32_t i = 0; i < tx_len; i++)
     {
-        fprintf(fuart, "%02X", (uint8_t)(tx[i] & 0xFF));
+        wp+=snprintf(wp,mxal-(wp-wout), "%02X", (uint8_t)(tx[i] & 0xFF));
     }
-    fprintf(fuart, "\"\r\n");
+    wp+=snprintf(wp,mxal-(wp-wout), "\"\r\n");
+    writeString(wout);
+    usleep(10000);
+    free(wout);
     if (at_expect(&response, "+CGLA: "))
     {
         goto err;
@@ -118,7 +198,7 @@ static int apdu_interface_transmit(struct euicc_ctx *ctx, uint8_t **rx, uint32_t
     {
         goto err;
     }
-
+    
     strtok(response, ",");
     hexstr = strtok(NULL, ",");
     if (!hexstr)
@@ -130,23 +210,23 @@ static int apdu_interface_transmit(struct euicc_ctx *ctx, uint8_t **rx, uint32_t
         hexstr++;
     }
     hexstr[strcspn(hexstr, "\"")] = '\0';
-
+    
     *rx_len = strlen(hexstr) / 2;
     *rx = malloc(*rx_len);
     if (!*rx)
     {
         goto err;
     }
-
+    
     ret = euicc_hexutil_hex2bin_r(*rx, *rx_len, hexstr, strlen(hexstr));
     if (ret < 0)
     {
         goto err;
     }
     *rx_len = ret;
-
+    
     goto exit;
-
+    
 err:
     fret = -1;
     free(*rx);
@@ -160,23 +240,29 @@ exit:
 static int apdu_interface_logic_channel_open(struct euicc_ctx *ctx, const uint8_t *aid, uint8_t aid_len)
 {
     char *response;
-
+    char obuf[1000];
+    int tail =0;
+    
     if (logic_channel)
     {
         return logic_channel;
     }
-
+    
     for (int i = 1; i <= 4; i++)
     {
-        fprintf(fuart, "AT+CCHC=%d\r\n", i);
+        tail = snprintf(obuf,999, "AT+CCHC=%d\r\n", i);
+        obuf[tail] = '\0';
+        writeString(obuf);
         at_expect(NULL, NULL);
     }
-    fprintf(fuart, "AT+CCHO=\"");
+    tail = snprintf(obuf,999, "AT+CCHO=\"");
     for (int i = 0; i < aid_len; i++)
     {
-        fprintf(fuart, "%02X", (uint8_t)(aid[i] & 0xFF));
+        tail+=snprintf(obuf+tail,999-tail, "%02X", (uint8_t)(aid[i] & 0xFF));
     }
-    fprintf(fuart, "\"\r\n");
+    tail+=snprintf(obuf+tail,999-tail, "\"\r\n");
+    obuf[tail] = '\0';
+    writeString(obuf);
     if (at_expect(&response, "+CCHO: "))
     {
         return -1;
@@ -186,30 +272,35 @@ static int apdu_interface_logic_channel_open(struct euicc_ctx *ctx, const uint8_
         return -1;
     }
     logic_channel = atoi(response);
-
+    
     return logic_channel;
 }
 
 static void apdu_interface_logic_channel_close(struct euicc_ctx *ctx, uint8_t channel)
 {
+    char obuf[1000];
+    int tail =0;
+    
     if (!logic_channel)
     {
         return;
     }
-    fprintf(fuart, "AT+CCHC=%d\r\n", logic_channel);
+    tail = snprintf(obuf,999, "AT+CCHC=%d\r\n", logic_channel);
+    obuf[tail] = '\0';
+    writeString(obuf);
     at_expect(NULL, NULL);
 }
 
 static int libapduinterface_init(struct euicc_apdu_interface *ifstruct)
 {
     memset(ifstruct, 0, sizeof(struct euicc_apdu_interface));
-
+    
     ifstruct->connect = apdu_interface_connect;
     ifstruct->disconnect = apdu_interface_disconnect;
     ifstruct->logic_channel_open = apdu_interface_logic_channel_open;
     ifstruct->logic_channel_close = apdu_interface_logic_channel_close;
     ifstruct->transmit = apdu_interface_transmit;
-
+    
     return 0;
 }
 
